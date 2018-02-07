d6a70666738add5fc9300e09b51bfead3383a58a
Pedro Arias Perez <parias@alpha07.aulas.gsyc.urjc.es>
2018-02-07 18:07:34 +0100

    Segunda version

--- servidor-random-http.py
+++ servidor-random-http.py
@@ -1,26 +1,20 @@
 #!/usr/bin/python3
 """
-Simple HTTP Server version 2: reuses the port, so it can be
-restarted right after it has been killed. Accepts connects from
-the outside world, by binding to the primary interface of the host.
+Ejercicio 14.3 URLs Aleatorias
 
-Jesus M. Gonzalez-Barahona and Gregorio Robles
-{jgb, grex} @ gsyc.es
-SAT and SARO subjects (Universidad Rey Juan Carlos)
+Pedro Arias Pérez
+p.ariaspe @ alumnos.urjc.es
+SAT , Universidad Rey Juan Carlos
 """
 
 import socket
 import random
 
 # Create a TCP objet socket and bind it to a port
-# Port should be 80, but since it needs root privileges,
-# let's use one above 1024
-
 mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Let the port be reused if no process is actually using it
 mySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-# Bind to the address corresponding to the main name of the host
-# mySocket.bind((socket.gethostname(), 1234))
+# Bind to the address corresponding to thelocalhost
 mySocket.bind(('localhost', 1234))
 
 # Queue a maximum of 5 TCP connection requests
@@ -39,12 +33,10 @@ try:
         num = random.randrange(999999999)
         url = 'http://localhost:1234/' + str(num)
         recvSocket.send(b"HTTP/1.1 200 OK\r\n\r\n" +
-                        b"<html><body><h1>Hola. " +
-                        b"<a href='" +
+                        b"<html><body><h1>Hola. <a href='" +
                         bytes(url, 'utf-8') +
                         b"'>Dame otra<a/>" +
-                        b"</body></h1></html>" +
-                        b"\r\n")
+                        b"</body></h1></html>\r\n")
         recvSocket.close()
 except KeyboardInterrupt:
     print("Closing binded socket")
